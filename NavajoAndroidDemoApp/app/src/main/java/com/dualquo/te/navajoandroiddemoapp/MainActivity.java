3407594a476f7ebd64a89965d1b3cb64155faac8
Marko Petrovic <marko@outbank.io>
2017-11-19 08:05:42 +0100

    initial commit of an empty lib and empty android projects

---
+++ NavajoAndroidDemoApp/app/src/main/java/com/dualquo/te/navajoandroiddemoapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dualquo.te.navajoandroiddemoapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * A sample Activity to showcase NavajoJava usage.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ NavajoAndroidDemoApp/navajojava/src/main/java/com/dualquo/te/navajojava/NavajoJava.java
@@ -0,0 +1,7 @@
+package com.dualquo.te.navajojava;
+
+/**
+ * Contains actual Password Validator & Strength Evaluator logic.
+ */
+public class NavajoJava {
+}
